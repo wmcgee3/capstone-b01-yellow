201ec8bf32558d2750c6d46ffc34f40f6803c279
wmcgee3 <61711986+wmcgee3@users.noreply.github.com>
2020-09-21 20:57:29 -0400

    Requirements clean up

--- nuts_and_bolts/__init__.py
+++ nuts_and_bolts/__init__.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-from flask_moment import Moment
 from flask_seeder import FlaskSeeder
 from nuts_and_bolts.config import Config
 from PIL import Image
@@ -14,7 +13,6 @@ login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 mail = Mail()
-moment = Moment()
 seeder = FlaskSeeder()
 try:
     logo = Image.open("static/images/logo.png")
@@ -30,7 +28,6 @@ def create_app(config_class=Config):
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
-    moment.init_app(app)
     seeder.init_app(app, db)
 
     with app.app_context():
