1c5293c65763ff3bb5ddb6520038defec967c4da
wmcgee3 <61711986+wmcgee3@users.noreply.github.com>
2020-09-26 12:28:23 -0400

    Moar trimming

--- nuts_and_bolts/main/routes.py
+++ nuts_and_bolts/main/routes.py
@@ -6,18 +6,3 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def home():
     return render_template('home.html')
-
-
-@main.route('/faq')
-def faq():
-    return render_template('faq.html')
-
-
-@main.route('/contact_us')
-def contact_us():
-    return render_template('contact_us.html')
-    
-
-@main.route('/products')
-def products():
-    return render_template('products.html')
