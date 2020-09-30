56c1ce163984bf24a8d33076e11ba1aaea569f6e
wmcgee3 <61711986+wmcgee3@users.noreply.github.com>
2020-09-30 04:23:45 -0400

    Update __init__.py

--- nuts_and_bolts/__init__.py
+++ nuts_and_bolts/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
 from flask_mail import Mail
 from flask_seeder import FlaskSeeder
 from nuts_and_bolts.config import Config
@@ -9,9 +8,6 @@ from PIL import Image
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
-login_manager = LoginManager()
-login_manager.login_view = 'users.login'
-login_manager.login_message_category = 'info'
 mail = Mail()
 seeder = FlaskSeeder()
 try:
@@ -26,7 +22,6 @@ def create_app(config_class=Config):
 
     db.init_app(app)
     bcrypt.init_app(app)
-    login_manager.init_app(app)
     mail.init_app(app)
     seeder.init_app(app, db)
 
