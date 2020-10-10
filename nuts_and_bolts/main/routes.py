6cc7f81f1a664aa3f7fc279a8b8894f6f5470be9
wmcgee3 <61711986+wmcgee3@users.noreply.github.com>
2020-10-10 00:40:41 -0400

    fixes

--- nuts_and_bolts/main/routes.py
+++ nuts_and_bolts/main/routes.py
@@ -1,6 +1,6 @@
-from datetime import datetime
-from flask import Blueprint, redirect, render_template, request, url_for
-from nuts_and_bolts.models import db, Button
+from flask import Blueprint, render_template
+from nuts_and_bolts import db
+from nuts_and_bolts.models import Products
 
 main = Blueprint('main', __name__)
 
@@ -8,3 +8,14 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def home():
     return render_template('home.html')
+
+
+@main.route('/contact_us')
+def contact_us():
+    return render_template('contact_us.html')
+
+
+@main.route('/product_list')
+def product_list():
+    products = db.session.query(Products).order_by(Products.sku)
+    return render_template('product_list.html', products=products)
