5ba3a0980928858df7c0f7143f816fb8bf82d950
KrBoldman <61923910+KrBoldman@users.noreply.github.com>
2020-10-16 13:50:56 -0400

    working
    it works. what do you want from me

--- nuts_and_bolts/management/routes.py
+++ nuts_and_bolts/management/routes.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, redirect, url_for
-from nuts_and_bolts.management.forms import InventoryForm
+from flask import Blueprint, render_template, redirect, url_for, flash
+from nuts_and_bolts.management.forms import InventoryForm, addproduct
 
 management = Blueprint('management', __name__)
 
@@ -7,5 +7,7 @@ management = Blueprint('management', __name__)
 def add_to_inventory():
     form = InventoryForm()
     if form.validate_on_submit():
-        return redirect(url_for('main.home'))
+        addproduct(name=form.name.data, desc=form.description.data, price=str(form.price.data), sku=form.sku.data, quantity=form.quantity.data)
+        flash(f'Entry created for {form.name.data}!', 'success')
+        return redirect(url_for('main.product_list'))
     return render_template('add_to_inventory.html', form=form)
