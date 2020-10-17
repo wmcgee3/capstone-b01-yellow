8917d4334efc40eadf3e940cf4cfb69b24436e79
wmcgee3 <61711986+wmcgee3@users.noreply.github.com>
2020-10-16 20:00:41 -0400

    refactoring

--- nuts_and_bolts/management/forms.py
+++ nuts_and_bolts/management/forms.py
@@ -1,24 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextField, IntegerField, DecimalField, SubmitField
 from wtforms.validators import DataRequired, Length, NumberRange, ValidationError
-from nuts_and_bolts.models import Products
-from nuts_and_bolts import db
 
 def sku_check(form, field):
     if len(str(field.data)) != 9:
-        raise ValidationError('sku must be 9 digits long!')
-
-def addproduct(name,desc,price,sku,quantity):
-    addition = Products(
-    name = name,
-    description = desc,
-    price = price,
-    sku = sku,
-    quantity = quantity,
-    )
-
-    db.session.add(addition)
-    db.session.commit()
+        raise ValidationError('sku must be 9 digits long!')    
+    
     
 class InventoryForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
@@ -27,5 +14,3 @@ class InventoryForm(FlaskForm):
     sku = IntegerField('sku', validators=[DataRequired(), sku_check])
     quantity = IntegerField('Quantity', validators=[DataRequired(), NumberRange(min=1, max=10000, message='quantity must be between 1 and 10,000')])
     submit = SubmitField('Submit')
-
-    
--- nuts_and_bolts/management/routes.py
+++ nuts_and_bolts/management/routes.py
@@ -1,13 +1,24 @@
 from flask import Blueprint, render_template, redirect, url_for, flash
-from nuts_and_bolts.management.forms import InventoryForm, addproduct
+from nuts_and_bolts.management.forms import InventoryForm
+from nuts_and_bolts.models import Products
+from nuts_and_bolts import db
 
 management = Blueprint('management', __name__)
+
 
 @management.route('/management/add_to_inventory', methods=['GET', 'POST'])
 def add_to_inventory():
     form = InventoryForm()
     if form.validate_on_submit():
-        addproduct(name=form.name.data, desc=form.description.data, price=str(form.price.data), sku=form.sku.data, quantity=form.quantity.data)
+        new_product = Products(
+            name=form.name.data,
+            desc=form.description.data,
+            price=str(form.price.data),
+            sku=form.sku.data,
+            quantity=form.quantity.data
+        )
+        db.session.add(new_product)
+        db.session.commit()
         flash(f'Entry created for {form.name.data}!', 'success')
         return redirect(url_for('main.product_list'))
     return render_template('add_to_inventory.html', form=form)
