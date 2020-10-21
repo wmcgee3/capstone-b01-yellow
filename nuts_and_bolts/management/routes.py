0e8f044d09e329cce32639722a6f9c8102c7b419
garrettnye <gnye@student.cscc.edu>
2020-10-20 23:32:12 -0400

    update inventory
    added button into products and tried to create route for form. got a little lost. taking a break for the night

--- nuts_and_bolts/management/routes.py
+++ nuts_and_bolts/management/routes.py
@@ -5,7 +5,7 @@ from nuts_and_bolts import db
 
 management = Blueprint('management', __name__)
 
-
+#add item to inventory
 @management.route('/management/add_to_inventory', methods=['GET', 'POST'])
 def add_to_inventory():
     form = InventoryForm()
@@ -23,7 +23,28 @@ def add_to_inventory():
         return redirect(url_for('main.product_list'))
     return render_template('add_to_inventory.html', form=form)
 
-#update inventory
-@management.route('/management/update_inventory')
-def update_inventory():
-    return render_template('update_inventory.html')
+#update inventory item 
+'''
+@management.route('/management/update_inventory/<int:sku>',  methods=['GET', 'POST'] )
+def update_inventory(sku):
+    form = InventoryForm()
+    if form.validate_on_submit():
+        new_product = Products(
+            name=form.name.data,
+            description=form.description.data,
+            price=form.price.data,
+            sku=int(form.sku.data),
+            quantity=int(form.quantity.data)
+        )
+
+        db.session.commit()
+        flash(f'Entry update for {form.name.data}!', 'success')
+        return redirect(url_for('main.product_list'))
+    elif request.method == 'GET':
+    form.name.data = product.name,
+    form.description.data = product.description,
+    form.price.data = product.price,
+    form.quantity.data = int(product.quantity),
+return render_template('update_inventory.html', form=form)
+
+'''   
