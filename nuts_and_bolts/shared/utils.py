a65c0ee75c279fae0c054331244c240d1a164cd1
wmcgee3 <61711986+wmcgee3@users.noreply.github.com>
2020-10-22 23:34:35 -0400

    ternary ftw

--- nuts_and_bolts/shared/utils.py
+++ nuts_and_bolts/shared/utils.py
@@ -3,8 +3,4 @@ from nuts_and_bolts import db
 from nuts_and_bolts.models import Products
 
 def get_cart():
-    if 'cart' in session:
-        _cart = session['cart']
-    else:
-        _cart = {}
-    return _cart
+    return session['cart'] if 'cart' in session else {}
