4fa13f4bb74a56115c0f9eeb2dd3a5d672c75666
wmcgee3 <61711986+wmcgee3@users.noreply.github.com>
2020-10-29 14:43:25 -0400

    Protect update/add to inventory. Added login/logout links

--- nuts_and_bolts/management/routes.py
+++ nuts_and_bolts/management/routes.py
@@ -1,12 +1,14 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
+from flask_login import login_required
 from nuts_and_bolts.management.forms import InventoryForm
 from nuts_and_bolts.models import Products
 from nuts_and_bolts import db
 
 management = Blueprint('management', __name__)
 
-#add item to inventory
+
 @management.route('/management/add_to_inventory', methods=['GET', 'POST'])
+@login_required
 def add_to_inventory():
     form = InventoryForm()
     if form.validate_on_submit():
@@ -24,18 +26,19 @@ def add_to_inventory():
     form.submit.label.text = 'Add'
     return render_template('add_to_inventory.html', form=form)
 
-#update inventory item 
-@management.route('/management/update_inventory/<int:product_id>',  methods=['GET', 'POST'] )
+
+@management.route('/management/update_inventory/<int:product_id>',  methods=['GET', 'POST'])
+@login_required
 def update_inventory(product_id):
     product = Products.query.get_or_404(product_id)
     form = InventoryForm()
     if request.method == 'POST':
         if form.validate_on_submit():
-            product.name=form.name.data
-            product.description=form.description.data
-            product.price=form.price.data
-            product.sku=int(form.sku.data)
-            product.quantity=int(form.quantity.data)
+            product.name = form.name.data
+            product.description = form.description.data
+            product.price = form.price.data
+            product.sku = int(form.sku.data)
+            product.quantity = int(form.quantity.data)
             db.session.commit()
             flash(f'Entry updated for ' + form.name.data + '!', 'success')
             return redirect(url_for('main.product_list'))
