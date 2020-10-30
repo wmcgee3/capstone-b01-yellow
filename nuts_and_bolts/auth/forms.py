cf1ed6f0ea861d22f4ff24cecf02c978ac9a680a
wmcgee3 <61711986+wmcgee3@users.noreply.github.com>
2020-10-30 19:24:19 -0400

    refactoring

--- nuts_and_bolts/auth/forms.py
+++ nuts_and_bolts/auth/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
