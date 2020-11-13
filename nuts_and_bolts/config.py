9d49f869edef1f81f115fb98d26f4a1a6c6cd6b0
Cbeachler <wbeachler@student.cscc.edu>
2020-11-12 19:47:17 -0500

    changed case
    Changed case for the variables to uppercase

--- nuts_and_bolts/config.py
+++ nuts_and_bolts/config.py
@@ -3,10 +3,10 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.environ.get('nab_key')
+    SECRET_KEY = os.environ.get('NAB_KEY')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('nab_user')
-    MAIL_PASSWORD = os.environ.get('nab_pass')
+    MAIL_USERNAME = os.environ.get('NAB_USER')
+    MAIL_PASSWORD = os.environ.get('NAB_PASS')
