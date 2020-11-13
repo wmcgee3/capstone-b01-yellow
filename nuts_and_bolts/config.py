f36f144f80672be07356e94a40a20821ceb9f5ff
wmcgee3 <61711986+wmcgee3@users.noreply.github.com>
2020-11-13 13:21:55 -0500

    Coercing more strings and updating db to match main branch

--- nuts_and_bolts/config.py
+++ nuts_and_bolts/config.py
@@ -8,5 +8,5 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('NAB_USER')
-    MAIL_PASSWORD = os.environ.get('NAB_PASS')
+    MAIL_USERNAME = str(os.environ.get('NAB_USER'))
+    MAIL_PASSWORD = str(os.environ.get('NAB_PASS'))
