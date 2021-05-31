c4fc146310765a4c074ba1ba24df3a2d20654883
wmcgee3 <61711986+wmcgee3@users.noreply.github.com>
2021-05-31 17:56:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import waitress
 
 from nuts_and_bolts import create_app
 
-waitress.serve(create_app(), host='127.0.0.1', port=0, url_scheme='https')
+waitress.serve(create_app(), host='127.0.0.1', port=5000, url_scheme='https')
